2ff3020cc1f2be0efd6642fd754e1eb34e309e77
Andrew Khorsun <andrewkhorsun@gmail.com>
2022-09-08 19:59:13 +0300

    add socket io client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+const socket = io("http://localhost:4000", {});
+
 function App() {
+  const [isConnected] = useState(socket.connected);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    socket.emit('start');
+    socket.on('ticker', function(response) {
+        const res = Array.isArray(response) ? response : [response];
+        // const json = res.map(item => JSON.stringify(item)).join('\n');
+        setData(res)
+    });
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p>{JSON.stringify(data[0]) }</p>
+          <p>Connected: {'' + isConnected}</p>
+        </div>
       </header>
     </div>
   );
